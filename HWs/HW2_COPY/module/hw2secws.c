722b9685f907ddf4432c1a6c0ad982509b89f5ed
Itay1007 <itay.barok@gmail.com>
2022-11-13 13:42:25 +0200

    edited file

--- HWs/HW2_COPY/module/hw2secws.c
+++ HWs/HW2_COPY/module/hw2secws.c
@@ -1,21 +1,21 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/netfilter.h>
-#include <linux/netfilter_ipv4.h>
+// #include <linux/netfilter.h>
+// #include <linux/netfilter_ipv4.h>
 #include <linux/fs.h>
 #include <linux/device.h>
 
-#define NF_DROP 0
-#define NF_ACCEPT 1
-#define ACCEPT_PACKET_MESSAGE "*** Packet Accepted ***"
-#define DROP_PACKET_MESSAGE "*** Packet Dropped ***"
+// #define NF_DROP 0
+// #define NF_ACCEPT 1
+// #define ACCEPT_PACKET_MESSAGE "*** Packet Accepted ***"
+// #define DROP_PACKET_MESSAGE "*** Packet Dropped ***"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Itay Barok");
 
-static struct nf_hook_ops *nf_net_forward_hook = NULL;
-static struct nf_hook_ops *nf_net_local_in_hook = NULL;
-static struct nf_hook_ops *nf_net_local_out_hook = NULL;
+// static struct nf_hook_ops *nf_net_forward_hook = NULL;
+// static struct nf_hook_ops *nf_net_local_in_hook = NULL;
+// static struct nf_hook_ops *nf_net_local_out_hook = NULL;
 
 static int major_number;
 static struct class* sysfs_class = NULL;
@@ -29,33 +29,33 @@ static struct file_operations fops = {
 
 // drop packets that go through the firewall to another host.
 // The firewall drops the packets and log it in kernel ring
-static unsigned int netfilter_forward_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
-	printk(KERN_INFO DROP_PACKET_MESSAGE);
-	return NF_DROP;
-}
+// static unsigned int netfilter_forward_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
+// 	printk(KERN_INFO DROP_PACKET_MESSAGE);
+// 	return NF_DROP;
+// }
 
 // accept packets that go in to the firewall to another host.
 // The firewall accepts the packets and log it in kernel ring
-static unsigned int netfilter_local_in_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
-	printk(KERN_INFO ACCEPT_PACKET_MESSAGE);
-	return NF_ACCEPT;
-}
+// static unsigned int netfilter_local_in_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
+// 	printk(KERN_INFO ACCEPT_PACKET_MESSAGE);
+// 	return NF_ACCEPT;
+// }
 
 // accept packets that go from the firewall to another host.
 // The firewall accepts the packets and log it in kernel ring
-static unsigned int netfilter_local_out_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
-	printk(KERN_INFO ACCEPT_PACKET_MESSAGE);
-	return NF_ACCEPT;
-}
+// static unsigned int netfilter_local_out_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
+// 	printk(KERN_INFO ACCEPT_PACKET_MESSAGE);
+// 	return NF_ACCEPT;
+// }
 
 // sysfs show function, the function that read from the attribute to the user
-ssize_t display_dropped_packets_counter(struct device *dev, struct device_attribute *attr, char *buf) {
+ssize_t display(struct device *dev, struct device_attribute *attr, char *buf) {
 	return scnprintf(buf, PAGE_SIZE, "%u\n", sysfs_int);
 }
 
 
 // sysfs store function, the function that writes to the attribute from the user
-ssize_t modify_dropped_packets_counter(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
+ssize_t modify(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
 	int temp;
 	if(sscanf(buf, "%u", &temp) == 1) {
 		sysfs_int = temp;
@@ -64,59 +64,59 @@ ssize_t modify_dropped_packets_counter(struct device *dev, struct device_attribu
 	return count;
 }
 
-static DEVICE_ATTR(sysfs_att, S_IWUSR | S_IRUGO, display_dropped_packets_counter, modify_dropped_packets_counter);
+static DEVICE_ATTR(sysfs_att, S_IWUSR | S_IRUGO, display, modify);
 
 // init function that is called when the module is loaded to the kernel
 static int __init my_module_init_function(void) {
-	nf_net_forward_hook = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
+	// nf_net_forward_hook = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
 	
-	nf_net_local_in_hook = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
+	// nf_net_local_in_hook = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
 	
-	nf_net_local_out_hook = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
+	// nf_net_local_out_hook = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
 
-	if(nf_net_forward_hook != NULL) {
-		nf_net_forward_hook->hook = (nf_hookfn*)netfilter_forward_hook;
-		nf_net_forward_hook->hooknum = NF_INET_FORWARD;
-		nf_net_forward_hook->pf = PF_INET;
-		nf_net_forward_hook->priority = 0;
+	// if(nf_net_forward_hook != NULL) {
+	// 	nf_net_forward_hook->hook = (nf_hookfn*)netfilter_forward_hook;
+	// 	nf_net_forward_hook->hooknum = NF_INET_FORWARD;
+	// 	nf_net_forward_hook->pf = PF_INET;
+	// 	nf_net_forward_hook->priority = 0;
 
-		if(nf_register_net_hook(&init_net, nf_net_forward_hook)) {
-			return -1;
-		}
-	}
-	else {
-		return -1;
-	}	
+	// 	if(nf_register_net_hook(&init_net, nf_net_forward_hook)) {
+	// 		return -1;
+	// 	}
+	// }
+	// else {
+	// 	return -1;
+	// }	
 
-	if(nf_net_local_in_hook != NULL) {
-		nf_net_local_in_hook->hook = (nf_hookfn*)netfilter_local_in_hook;
-		nf_net_local_in_hook->hooknum = NF_INET_LOCAL_IN;
-		nf_net_local_in_hook->pf = PF_INET;
-		nf_net_local_in_hook->priority = 0;
+	// if(nf_net_local_in_hook != NULL) {
+	// 	nf_net_local_in_hook->hook = (nf_hookfn*)netfilter_local_in_hook;
+	// 	nf_net_local_in_hook->hooknum = NF_INET_LOCAL_IN;
+	// 	nf_net_local_in_hook->pf = PF_INET;
+	// 	nf_net_local_in_hook->priority = 0;
 
-		if(nf_register_net_hook(&init_net, nf_net_local_in_hook)) {
-			return -1;	
-		}
-	}
-	else {
-		return -1;
-	}	
+	// 	if(nf_register_net_hook(&init_net, nf_net_local_in_hook)) {
+	// 		return -1;	
+	// 	}
+	// }
+	// else {
+	// 	return -1;
+	// }	
 
-	if(nf_net_local_out_hook != NULL) {
-		nf_net_local_out_hook->hook = (nf_hookfn*)netfilter_local_out_hook;
-		nf_net_local_out_hook->hooknum = NF_INET_LOCAL_OUT;
-		nf_net_local_out_hook->pf = PF_INET;
-		nf_net_local_out_hook->priority = 0;
+	// if(nf_net_local_out_hook != NULL) {
+	// 	nf_net_local_out_hook->hook = (nf_hookfn*)netfilter_local_out_hook;
+	// 	nf_net_local_out_hook->hooknum = NF_INET_LOCAL_OUT;
+	// 	nf_net_local_out_hook->pf = PF_INET;
+	// 	nf_net_local_out_hook->priority = 0;
 
-		if(nf_register_net_hook(&init_net, nf_net_local_out_hook)) {
-			return -1;
-		}
-	}
-	else {
-		return -1;
-	}
+	// 	if(nf_register_net_hook(&init_net, nf_net_local_out_hook)) {
+	// 		return -1;
+	// 	}
+	// }
+	// else {
+	// 	return -1;
+	// }
 
-	major_number = register_chrdev(0, "Sysfs_Device", &fops);
+	major_number = register_chrdev(0, "Sysfs_Device", &fops);\
 	if(major_number < 0) {
 		return -1;
 	}
@@ -146,20 +146,20 @@ static int __init my_module_init_function(void) {
 
 // exit function that is called when the module is removed from the kernel
 static void __exit my_module_exit_function(void) {
-	if(nf_net_forward_hook != NULL) {
-		nf_unregister_net_hook(&init_net, nf_net_forward_hook);
-		kfree(nf_net_forward_hook);
-	}
+	// if(nf_net_forward_hook != NULL) {
+	// 	nf_unregister_net_hook(&init_net, nf_net_forward_hook);
+	// 	kfree(nf_net_forward_hook);
+	// }
 
-	if(nf_net_local_in_hook != NULL) {
-		nf_unregister_net_hook(&init_net, nf_net_local_in_hook);
-		kfree(nf_net_local_in_hook);
-	}
+	// if(nf_net_local_in_hook != NULL) {
+	// 	nf_unregister_net_hook(&init_net, nf_net_local_in_hook);
+	// 	kfree(nf_net_local_in_hook);
+	// }
 
-	if(nf_net_local_out_hook != NULL) {
-		nf_unregister_net_hook(&init_net, nf_net_local_out_hook);
-		kfree(nf_net_local_out_hook);
-	}
+	// if(nf_net_local_out_hook != NULL) {
+	// 	nf_unregister_net_hook(&init_net, nf_net_local_out_hook);
+	// 	kfree(nf_net_local_out_hook);
+	// }
 
 	device_remove_file(sysfs_device, (const struct device_attribute *)&dev_attr_sysfs_att.attr);
 	device_destroy(sysfs_class, MKDEV(major_number, 0));
