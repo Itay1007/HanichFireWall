d89732941a053994674ac31288c67be17485329d
Itay1007 <itay.barok@gmail.com>
2022-11-13 13:54:42 +0200

    edit file

--- HWs/HW2_COPY/module/hw2secws.c
+++ HWs/HW2_COPY/module/hw2secws.c
@@ -1,19 +1,19 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-// #include <linux/netfilter.h>
-// #include <linux/netfilter_ipv4.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
 #include <linux/fs.h>
 #include <linux/device.h>
 
-// #define NF_DROP 0
-// #define NF_ACCEPT 1
-// #define ACCEPT_PACKET_MESSAGE "*** Packet Accepted ***"
-// #define DROP_PACKET_MESSAGE "*** Packet Dropped ***"
+#define NF_DROP 0
+#define NF_ACCEPT 1
+#define ACCEPT_PACKET_MESSAGE "*** Packet Accepted ***"
+#define DROP_PACKET_MESSAGE "*** Packet Dropped ***"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Itay Barok");
 
-// static struct nf_hook_ops *nf_net_forward_hook = NULL;
+static struct nf_hook_ops *nf_net_forward_hook = NULL;
 // static struct nf_hook_ops *nf_net_local_in_hook = NULL;
 // static struct nf_hook_ops *nf_net_local_out_hook = NULL;
 
@@ -29,10 +29,10 @@ static struct file_operations fops = {
 
 // drop packets that go through the firewall to another host.
 // The firewall drops the packets and log it in kernel ring
-// static unsigned int netfilter_forward_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
-// 	printk(KERN_INFO DROP_PACKET_MESSAGE);
-// 	return NF_DROP;
-// }
+static unsigned int netfilter_forward_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
+	printk(KERN_INFO DROP_PACKET_MESSAGE);
+	return NF_DROP;
+}
 
 // accept packets that go in to the firewall to another host.
 // The firewall accepts the packets and log it in kernel ring
@@ -68,25 +68,25 @@ static DEVICE_ATTR(sysfs_att, S_IWUSR | S_IRUGO, display, modify);
 
 // init function that is called when the module is loaded to the kernel
 static int __init my_module_init_function(void) {
-	// nf_net_forward_hook = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
+	nf_net_forward_hook = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
 	
 	// nf_net_local_in_hook = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
 	
 	// nf_net_local_out_hook = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
 
-	// if(nf_net_forward_hook != NULL) {
-	// 	nf_net_forward_hook->hook = (nf_hookfn*)netfilter_forward_hook;
-	// 	nf_net_forward_hook->hooknum = NF_INET_FORWARD;
-	// 	nf_net_forward_hook->pf = PF_INET;
-	// 	nf_net_forward_hook->priority = 0;
+	if(nf_net_forward_hook != NULL) {
+		nf_net_forward_hook->hook = (nf_hookfn*)netfilter_forward_hook;
+		nf_net_forward_hook->hooknum = NF_INET_FORWARD;
+		nf_net_forward_hook->pf = PF_INET;
+		nf_net_forward_hook->priority = 0;
 
-	// 	if(nf_register_net_hook(&init_net, nf_net_forward_hook)) {
-	// 		return -1;
-	// 	}
-	// }
-	// else {
-	// 	return -1;
-	// }	
+		if(nf_register_net_hook(&init_net, nf_net_forward_hook)) {
+			return -1;
+		}
+	}
+	else {
+		return -1;
+	}	
 
 	// if(nf_net_local_in_hook != NULL) {
 	// 	nf_net_local_in_hook->hook = (nf_hookfn*)netfilter_local_in_hook;
@@ -146,10 +146,10 @@ static int __init my_module_init_function(void) {
 
 // exit function that is called when the module is removed from the kernel
 static void __exit my_module_exit_function(void) {
-	// if(nf_net_forward_hook != NULL) {
-	// 	nf_unregister_net_hook(&init_net, nf_net_forward_hook);
-	// 	kfree(nf_net_forward_hook);
-	// }
+	if(nf_net_forward_hook != NULL) {
+		nf_unregister_net_hook(&init_net, nf_net_forward_hook);
+		kfree(nf_net_forward_hook);
+	}
 
 	// if(nf_net_local_in_hook != NULL) {
 	// 	nf_unregister_net_hook(&init_net, nf_net_local_in_hook);
