9f5ce9d063ec68f29fc2f8b7ef31583b6ac1d51a
Itay Barok <itay.barok@gmail.com>
2022-12-10 21:22:15 +0200

    add print add print add print add print

--- HWs/HW3/user/main.c
+++ HWs/HW3/user/main.c
@@ -119,8 +119,8 @@ void parse_line_to_rule(rule_t *rule_ptr, char* rule_chars_line) {
                     }
                     printf("fill ip/mask\n");
                     fill_ip_mask(ip, mask, rule_line_token);
-                    printf("ip=%s", ip);
-                    printf("mask=%s", mask);
+                    printf("ip=%s\n", ip);
+                    printf("mask=%s\n", mask);
                     be_ip_number = make_be_ip_number(ip);
                     rule_ptr->src_ip = be_ip_number;
                     mask_size = atoi(mask);
