e51b9d3ef8b6a02c40d7901d62137cb2ea7be893
Itay Barok <itay.barok@gmail.com>
2022-12-11 09:47:36 +0200

    less prints

--- HWs/HW3/user/main.c
+++ HWs/HW3/user/main.c
@@ -66,11 +66,9 @@ void parse_line_to_rule(rule_t *rule_ptr, char* rule_chars_line) {
 
     for(rule_element_i = 0; rule_element_i < 9; rule_element_i) {
         for(; rule_chars_line[i] == ' ' || rule_chars_line[i] == '\0' || rule_chars_line[i] == '\n'; i++) {
-            printf("space in char index: %d: %c: %d\n", i, rule_chars_line[i], rule_chars_line[i]);
         }
 
         for(j = 0; rule_chars_line[i] != ' ' && rule_chars_line[i] != '\0' && rule_chars_line[i] != '\n'; i++, j++) {
-            printf("non space in char index: %d: %c: %d\n", i, rule_chars_line[i], rule_chars_line[i]);
             rule_line_token[j] = rule_chars_line[i];
         }
         rule_line_token[j] = '\0';
