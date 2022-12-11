308eecbe3d81f3e97ebc24b4411b16a4537e0ae7
Itay Barok <itay.barok@gmail.com>
2022-12-11 21:19:44 +0200

    refactor things

--- HWs/HW3/module/static_rules_table.c
+++ HWs/HW3/module/static_rules_table.c
@@ -35,7 +35,7 @@ void add_static_table_rule(const char __user *user_space_buf) {
 	// 	printk(KERN_INFO "%i-%c-%d\t", i, ((char *)rule_ptr)[i], ((char *)rule_ptr)[i]);
 	// }
 
-	print_rule_kernel_mode(rule_ptr);
+	print_rule_kernel_mode(&static_rules_table[number_of_rules_in_table]);
 	number_of_rules_in_table++;
 }
 
