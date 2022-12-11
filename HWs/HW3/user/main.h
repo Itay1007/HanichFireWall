efb766553bf1dfa33a890c056eb2b7335ff1052f
Itay Barok <itay.barok@gmail.com>
2022-12-11 09:56:59 +0200

    bugfix

--- HWs/HW3/user/main.h
+++ HWs/HW3/user/main.h
@@ -1,6 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include "validation.h"
+#include "utils.h"
+
 void load_rules(char *path_to_rules_file);
 void parse_line_to_rule(rule_t *rule_ptr, char* rule_chars_line);
 void show_rules(void);
